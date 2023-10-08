7d4d42f43542da109a531431e634e62aa7483099
tushar-gopale <tushargopale12@gmail.com>
2023-10-08 17:59:44 +0530

    update functionality added#

--- src/main/java/com/tushardev/attendencesystem/controller/UserController.java
+++ src/main/java/com/tushardev/attendencesystem/controller/UserController.java
@@ -3,11 +3,9 @@ package com.tushardev.attendencesystem.controller;
 import com.tushardev.attendencesystem.entity.User;
 import com.tushardev.attendencesystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -20,6 +18,14 @@ public class UserController {
     public ResponseEntity<?> addUser(@RequestBody User user){
 
        Boolean response= userService.addUser(user);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PutMapping("/update-user")
+    public ResponseEntity<?> updateUser( @RequestBody User user){
+
+        Boolean response= userService.updateUser(user);
 
         return ResponseEntity.ok().body(response);
     }
--- src/main/java/com/tushardev/attendencesystem/dao/UserRepo.java
+++ src/main/java/com/tushardev/attendencesystem/dao/UserRepo.java
@@ -4,4 +4,5 @@ import com.tushardev.attendencesystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
+
 }
